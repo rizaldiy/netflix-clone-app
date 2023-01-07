2406d7fbd6e393136acd9dd62292ecb5f7b03306
Rizaldi Yusuf <zalizaldiy44@gmail.com>
2023-01-08 02:44:55 +0700

    Update Thumbnail.tsx

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -21,7 +21,7 @@ function Thumbnail({ movie }: Props) {
           movie.backdrop_path || movie.poster_path
         }`}
         className="rounded-sm object-cover md:rounded"
-        layout = "fill"
+        layout="fill"
       />
     </div>
   )
